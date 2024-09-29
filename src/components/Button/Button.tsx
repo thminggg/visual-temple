d58411545d2084375c23ef0170b49ad6620f215b
thminggg <tsehongming21@gmail.com>
2024-09-29 00:08:41 -0700

    dev: update button taking onclick handler

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,15 @@
 export const Button = ({
   text,
   className,
+  handleClick,
 }: {
   text: string;
   className?: string;
+  handleClick?: () => void;
 }) => (
   <button
     className={`bg-accent text-white font-semibold min-w-[200px] min-h-[60px] rounded-xl ${className}`}
+    onClick={handleClick}
   >
     {text}
   </button>
